3c1cd898f2ddd893fddf04a8d7aff4c3344f539c
bakker4444 <bakker4444@gmail.com>
2018-05-15 20:30:19 -0700

    adding successful signup test and invalid signup test

--- apps/accounts/tests.py
+++ apps/accounts/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse, resolve
 from django.test import TestCase
@@ -23,3 +24,53 @@ class SignUpTests(TestCase):
     def test_contains_form(self):
         form = self.response.context.get("form")
         self.assertIsInstance(form, UserCreationForm)
+
+
+class SuccessfulSignUpTests(TestCase):
+    def setUp(self):
+        url = reverse("signup")
+        data = {
+            "username": "john",
+            "password1": "abcdef123456",
+            "password2": "abcdef123456"
+        }
+        self.response = self.client.post(url, data)
+        self.index_url = reverse("index")
+
+    def test_redirection(self):
+        """
+        A valid form submission should redirect the user to the index page
+        """
+        self.assertRedirects(self.response, self.index_url)
+
+    def test_user_creation(self):
+        self.assertTrue(User.objects.exists())
+
+    def test_user_authentication(self):
+        """
+        Create a new request to an arbitrary page.
+        The resulting response should now have a `user` to its context,
+        after a successful sign up.
+        """
+        response = self.client.get(self.index_url)
+        user = response.context.get("user")
+        self.assertTrue(user.is_authenticated)
+
+
+class InvalidSignUpTests(TestCase):
+    def setUp(self):
+        url = reverse("signup")
+        self.response = self.client.post(url, {})  # submit an empty dictionary
+
+    def test_signup_status_code(self):
+        """
+        An invalid form submission should return to the same page
+        """
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_form_errors(self):
+        form = self.response.context.get("form")
+        self.assertTrue(form.errors)
+
+    def test_dont_create_user(self):
+        self.assertFalse(User.objects.exists())
