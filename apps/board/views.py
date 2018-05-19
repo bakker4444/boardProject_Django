2e0be7bae6afb7a83a1f1b6c4b8b7d48b0608962
bakker4444 <bakker4444@gmail.com>
2018-05-18 18:28:11 -0700

    change from temp authorized user to user querying from database

--- apps/board/views.py
+++ apps/board/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
+
 from .forms import NewTopicForm
 from .models import Board, Topic, Post
 
@@ -22,18 +23,17 @@ def board_topics(request, pk):
 @login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    user = User.objects.first()     # TODO: get tht currently logged in user
     if request.method == "POST":
         form = NewTopicForm(request.POST)
         if form.is_valid():
             topic = form.save(commit=False)
             topic.board = board
-            topic.starter = user
+            topic.starter = request.user
             topic.save()
             post = Post.objects.create(
                 message=form.cleaned_data.get("message"),
                 topic=topic,
-                created_by=user
+                created_by=request.user
             )
             return redirect("board_topics", pk=board.pk)        # TODO: redirect to the created topic page
     else:
