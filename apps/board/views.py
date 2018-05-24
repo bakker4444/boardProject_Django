4a76baa8278836b406593093aa4738a7a2e10926
bakker4444 <bakker4444@gmail.com>
2018-05-23 18:22:34 -0700

    add feature, gravatar picture, pagination change, view count based on session id, page number show up under the topic title, etc

--- apps/board/views.py
+++ apps/board/views.py
@@ -7,6 +7,7 @@ from django.db.models import Count
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 
 from .forms import NewTopicForm, PostForm
 from .models import Board, Topic, Post
@@ -23,7 +24,7 @@ class TopicListView(ListView):
     model = Topic
     context_object_name = "topics"
     template_name = "topics.html"
-    paginate_by = 10
+    paginate_by = 20
 
     def get_context_data(self, **kwargs):
         kwargs["board"] = self.board
@@ -39,11 +40,14 @@ class PostListView(ListView):
     model = Post
     context_object_name = "posts"
     template_name = "topic_posts.html"
-    paginate_by = 2
+    paginate_by = 20
 
     def get_context_data(self, **kwargs):
-        self.topic.views += 1
-        self.topic.save()
+        session_key = 'viewed_topic_{}'.format(self.topic.pk)
+        if not self.request.session.get(session_key, False):
+            self.topic.views += 1
+            self.topic.save()
+            self.request.session[session_key] = True
         kwargs["topic"] = self.topic
         return super().get_context_data(**kwargs)
 
@@ -63,7 +67,7 @@ def new_topic(request, pk):
             topic.board = board
             topic.starter = request.user
             topic.save()
-            post = Post.objects.create(
+            Post.objects.create(
                 message=form.cleaned_data.get("message"),
                 topic=topic,
                 created_by=request.user
@@ -88,7 +92,19 @@ def reply_topic(request, pk, topic_pk):
             post.topic = topic
             post.created_by = request.user
             post.save()
-            return redirect("topic_posts", pk=pk, topic_pk=topic_pk)
+
+            topic.updated_at = timezone.now()
+            topic.save()
+
+            topic_url = reverse("topic_posts", kwargs={"pk": pk, "topic_pk": topic_pk})
+            topic_post_url = "{url}?page={page}#{id}".format(
+                url=topic_url,
+                id=post.pk,
+                page=topic.get_page_count()
+            )
+
+            return redirect(topic_post_url)
+
     else:
         form = PostForm()
     context = {
